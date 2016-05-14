d7a73e0390d2f77cf33d9da0c74964511706bef5
mswift42 <martin.haesler@gmail.com>
2016-05-14 11:06:29 +0100

    convert json result to recipe instances

--- lib/services/RecipeService.dart
+++ lib/services/RecipeService.dart
@@ -1,10 +1,12 @@
 import 'package:angularattack2016_mswift42/services/Recipe.dart';
 import 'dart:html';
+import 'dart:convert';
 import 'package:angular2/core.dart';
 
 @Injectable()
 class RecipeService {
   final String _food2forkapikey = '7987c43afcf8a03a964bbcb0c9152c84';
+  List<Recipe> recipes = [];
 
   String get food2forkapikey => _food2forkapikey;
 
@@ -23,8 +25,10 @@ class RecipeService {
   }
 
   void onDataLoaded(String response) {
-    print(response);
-  }
+  List<String> rec = JSON.decode(response)["recipes"];
+    rec.forEach((i)=> recipes.add(new Recipe(i["publisher"], i["title"],
+        i["source_url"], i["image_url"], i["publisher_url"])));
+ }
 
 }
 
