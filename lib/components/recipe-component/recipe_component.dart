73ec941da390a6dea539a3fe7f76d43c5a5ebc85
mswift42 <martin.haesler@gmail.com>
2016-05-14 12:19:19 +0100

    Import and display titlecomponent

--- lib/components/recipe-component/recipe_component.dart
+++ lib/components/recipe-component/recipe_component.dart
@@ -2,12 +2,13 @@ import 'package:angular2/core.dart';
 import 'package:angularattack2016_mswift42/services/Recipe.dart';
 import 'package:angularattack2016_mswift42/services/RecipeService.dart';
 import 'package:angularattack2016_mswift42/components/recipeimage-component/recipeimage_component.dart';
+import 'package:angularattack2016_mswift42/components/title-component/title_component.dart';
 
 @Component(
     selector: 'recipe-component',
 templateUrl: 'recipe_component.html',
 styleUrls: const ['recipe_component.css'],
-directives: const [RecipeimageComponent]
+directives: const [RecipeimageComponent, TitleComponent]
 )
 
 class RecipeComponent {
