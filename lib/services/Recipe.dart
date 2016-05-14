7b6605417d04b612ffd98bbbfd0eb9bcaf04a6b6
mswift42 <martin.haesler@gmail.com>
2016-05-14 08:19:31 +0100

    Add class Recipe dart

---
+++ lib/services/Recipe.dart
@@ -0,0 +1,6 @@
+class Recipe {
+  String publisher, title, source_url, image_url, publisher_url;
+
+  Recipe(this.publisher, this.title, this.source_url, this.image_url,
+      this.publisher_url);
+}
