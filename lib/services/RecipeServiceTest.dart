cde4e143a1123692590095c6edd6edc6fd9395cb
mswift42 <martin.haesler@gmail.com>
2016-05-14 09:08:24 +0100

    add more tests for querySearch

--- lib/services/RecipeServiceTest.dart
+++ lib/services/RecipeServiceTest.dart
@@ -13,5 +13,9 @@ void main() {
   test('expect querySearch to return correct query string', () {
    var q1 = recservice.queryUrl("bacon");
     expect(q1, "http://food2fork.com/api/search?key=7987c43afcf8a03a964bbcb0c9152c84&q=bacon");
+    var q2 = recservice.queryUrl("french toast");
+    expect(q2,"http://food2fork.com/api/search?key=7987c43afcf8a03a964bbcb0c9152c84&q=french&toast");
+    var q3 = recservice.queryUrl("french toast bacon");
+    expect(q3, "http://food2fork.com/api/search?key=7987c43afcf8a03a964bbcb0c9152c84&q=french&toast&bacon");
   });
 }
