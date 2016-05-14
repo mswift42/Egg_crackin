c78d25b0c808ce7cafa4c65023231d9f4476d8cf
mswift42 <martin.haesler@gmail.com>
2016-05-14 09:17:46 +0100

    Import and display RecipeComponent

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -3,7 +3,12 @@
 
 import 'package:angular2/core.dart';
 import 'package:angularattack2016_mswift42/components/navbar-component/navbar_component.dart';
+import 'package:angularattack2016_mswift42/components/recipe-component/recipe_component.dart';
+import 'package:angularattack2016_mswift42/services/RecipeService.dart';
 
-@Component(selector: 'my-app', templateUrl: 'app_component.html',
-    directives: const [NavbarComponent])
+@Component(
+    selector: 'my-app',
+    templateUrl: 'app_component.html',
+    providers: const [RecipeService],
+    directives: const [NavbarComponent, RecipeComponent])
 class AppComponent {}
