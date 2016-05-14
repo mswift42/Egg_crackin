bed70e44999e834e0807ff9ddc91bef2e9c8c133
mswift42 <martin.haesler@gmail.com>
2016-05-14 08:48:56 +0100

    Add test file for RecipeService

---
+++ lib/services/RecipeServiceTest.dart
@@ -0,0 +1,17 @@
+import 'package:test/test.dart';
+import 'package:angularattack2016_mswift42/services/RecipeService.dart';
+
+
+void main() {
+  var recservice = new RecipeService();
+  test('apikey is a string', () {
+   expect(recservice.food2forkapikey is String, true);
+  });
+  test('expect querySearch to return a string' , () {
+   expect(recservice.queryUrl("hallo") is String, true);
+  });
+  test('expect querySearch to return correct query string', () {
+   var q1 = recservice.queryUrl("bacon");
+    expect(q1, "http://food2fork.com/api/search?key=7987c43afcf8a03a964bbcb0c9152c84&q=bacon");
+  });
+}
