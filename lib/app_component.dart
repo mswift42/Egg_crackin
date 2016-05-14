956c3b9b373fcaceee040c1da6372b7ff95abc95
mswift42 <martin.haesler@gmail.com>
2016-05-14 07:47:32 +0100

    Import and display navbar-component

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,6 +2,8 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/core.dart';
+import 'package:angularattack2016_mswift42/components/navbar-component/navbar_component.dart';
 
-@Component(selector: 'my-app', templateUrl: 'app_component.html')
+@Component(selector: 'my-app', templateUrl: 'app_component.html',
+    directives: const [NavbarComponent])
 class AppComponent {}
