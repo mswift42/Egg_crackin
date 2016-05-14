fcec24b8c5bec5a7073384894128110759d76891
mswift42 <martin.haesler@gmail.com>
2016-05-14 12:03:56 +0100

    Add publisher-component folder

---
+++ lib/components/navbar-component/navbar_component.dart
@@ -0,0 +1,7 @@
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'navbar-component',
+    templateUrl: 'navbar_component.html',
+    styleUrls: const ['navbar_component.css'])
+class NavbarComponent {}
--- lib/components/recipe-component/recipe_component.dart
+++ lib/components/recipe-component/recipe_component.dart
@@ -1,19 +1,24 @@
 import 'package:angular2/core.dart';
 import 'package:angularattack2016_mswift42/services/Recipe.dart';
 import 'package:angularattack2016_mswift42/services/RecipeService.dart';
+import 'package:angularattack2016_mswift42/components/recipeimage-component/recipeimage_component.dart';
 
 @Component(
     selector: 'recipe-component',
 templateUrl: 'recipe_component.html',
-styleUrls: const ['recipe_component.css']
+styleUrls: const ['recipe_component.css'],
+directives: const [RecipeimageComponent]
 )
 
 class RecipeComponent {
   final RecipeService _recservice;
+  List<Recipe> recipes;
 
   RecipeComponent(this._recservice);
 
   searchRecipes(String value) {
     _recservice.loadData(value);
+    recipes = _recservice.recipes;
+    recipes.forEach((i) => print(i.image_url));
   }
 }
