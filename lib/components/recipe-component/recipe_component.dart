79359c8a47988b0330b1e45db910e580165d2591
mswift42 <martin.haesler@gmail.com>
2016-05-14 09:10:41 +0100

    add component details

---
+++ lib/components/recipe-component/recipe_component.dart
@@ -0,0 +1,13 @@
+import 'package:angular2/core.dart';
+import 'package:angularattack2016_mswift42/services/Recipe.dart';
+import 'package:angularattack2016_mswift42/services/RecipeService.dart';
+
+@Component(
+    selector: 'recipe-component',
+templateUrl: 'recipe_component.html',
+styleUrls: const ['recipe_component.css']
+)
+
+class RecipeComponent {
+
+}
