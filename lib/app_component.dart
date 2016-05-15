9d130e019655c90ef8ee7356d5743c549e4ae7c2
mswift42 <martin.haesler@gmail.com>
2016-05-15 15:19:22 +0100

    Add RouteConfig

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,14 +1,31 @@
-// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
 import 'package:angularattack2016_mswift42/components/navbar-component/navbar_component.dart';
 import 'package:angularattack2016_mswift42/components/recipe-component/recipe_component.dart';
 import 'package:angularattack2016_mswift42/services/RecipeService.dart';
+import 'package:angularattack2016_mswift42/services/FavouritesService.dart';
+import 'package:angularattack2016_mswift42/components/publisherlist-component/publisherlist_component.dart';
+import 'package:angularattack2016_mswift42/components/favourite-component/favourite_component.dart';
 
 @Component(
     selector: 'my-app',
     templateUrl: 'app_component.html',
-    providers: const [RecipeService],
-    directives: const [NavbarComponent, RecipeComponent])
+    providers: const [
+      RecipeService,
+      FavouritesService
+    ],
+    directives: const [
+      NavbarComponent,
+      RecipeComponent,
+      PublisherlistComponent
+    ])
+@RouteConfig(const [
+  const Route(
+      path: '/',
+      name: 'Recipes',
+      component: RecipeComponent,
+      useAsDefault: true),
+  const Route(
+      path: '/favourites', name: 'Favourites', component: FavouriteComponent)
+])
 class AppComponent {}
