fae61e487eaafd443fc6fde4d60c768a5f3b2b5b
mswift42 <martin.haesler@gmail.com>
2016-06-20 12:06:24 +0100

    Move tooltip into seperate component

--- lib/components/favourite-component/favourite_component.dart
+++ lib/components/favourite-component/favourite_component.dart
@@ -1,11 +1,13 @@
 import 'package:angular2/core.dart';
 import 'package:angularattack2016_mswift42/services/FavouritesService.dart';
 import 'package:angularattack2016_mswift42/services/Recipe.dart';
+import 'package:angularattack2016_mswift42/components/tooltip-component/tooltip_component.dart';
 
 @Component(
     selector: 'favourite-component',
     templateUrl: 'favourite_component.html',
-    styleUrls: const ['favourite_component.css'])
+    styleUrls: const ['favourite_component.css'],
+    directives: const [TooltipComponent])
 class FavouriteComponent {
   @Input()
   String title;
