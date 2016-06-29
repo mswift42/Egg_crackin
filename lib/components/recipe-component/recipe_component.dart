dc57cb67632adb60f486743adb1ca942a8c5f0d0
mswift42 <martin.haesler@gmail.com>
2016-06-29 18:07:29 +0100

    Store last search when submitting

--- lib/components/recipe-component/recipe_component.dart
+++ lib/components/recipe-component/recipe_component.dart
@@ -1,6 +1,7 @@
 import 'package:angular2/core.dart';
 import 'package:angularattack2016_mswift42/services/Recipe.dart';
 import 'package:angularattack2016_mswift42/services/RecipeService.dart';
+import 'package:angularattack2016_mswift42/services/SearchhistoryService.dart';
 import 'package:angularattack2016_mswift42/components/recipeimage-component/recipeimage_component.dart';
 import 'package:angularattack2016_mswift42/components/title-component/title_component.dart';
 import 'package:angularattack2016_mswift42/components/publisherurl-component/publisherurl_component.dart';
@@ -20,13 +21,15 @@ import 'package:angularattack2016_mswift42/components/favourite-component/favour
     ])
 class RecipeComponent {
   final RecipeService _recservice;
+  final SearchhistoryService _searchhistoryservice;
   List<Recipe> recipes;
 
-  RecipeComponent(this._recservice);
+  RecipeComponent(this._recservice, this._searchhistoryservice);
 
   searchRecipes(String value) {
     _recservice.recipes = [];
     _recservice.loadData(value);
     recipes = _recservice.recipes;
+    _searchhistoryservice.saveSearch(value);
   }
 }
