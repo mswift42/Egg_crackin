37bf851a98822d4ed5a5b84d8a95cd404b8788a3
mswift42 <martin.haesler@gmail.com>
2016-06-29 18:01:42 +0100

    add saveToStorage method

--- lib/services/SearchhistoryService.dart
+++ lib/services/SearchhistoryService.dart
@@ -1,16 +1,25 @@
 import 'package:angular2/core.dart';
+import 'dart:html';
+import 'dart:convert';
 
 @Injectable()
 class SearchhistoryService {
   List<String> _searchhistory;
+  Storage localStorage = window.localStorage;
+  final _storagename = "searchhistory";
 
   void saveSearch(String query) {
     if (!(_searchhistory.contains(query))) {
-    _searchhistory.insert(0,query);
+      _searchhistory.insert(0, query);
+      saveToStorage();
       if (_searchhistory.length > 10) {
         _searchhistory.removeLast();
+        saveToStorage();
       }
     }
   }
 
+  void saveToStorage() {
+   localStorage[_storagename] = JSON.encode(_searchhistory);
+  }
 }
