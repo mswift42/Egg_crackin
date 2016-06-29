e67f9afd4ac72b63941f0bdfaa0fe5d5a53d2d41
mswift42 <martin.haesler@gmail.com>
2016-06-29 17:49:37 +0100

    if searchhistory > 10 remove last element

--- lib/services/SearchhistoryService.dart
+++ lib/services/SearchhistoryService.dart
@@ -2,9 +2,15 @@ import 'package:angular2/core.dart';
 
 @Injectable()
 class SearchhistoryService {
-  Set<String> _searchhistory;
+  List<String> _searchhistory;
 
   void saveSearch(String query) {
-    _searchhistory.add(query);
+    if (!(_searchhistory.contains(query))) {
+    _searchhistory.insert(0,query);
+      if (_searchhistory.length > 10) {
+        _searchhistory.removeLast();
+      }
+    }
   }
+
 }
