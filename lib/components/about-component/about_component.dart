4009d81fd99aab127d1ff544dd64eee71174e9b4
mswift42 <martin.haesler@gmail.com>
2016-08-03 17:34:41 +0100

    Add component selector

---
+++ lib/components/about-component/about_component.dart
@@ -0,0 +1,7 @@
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'about-component',
+    templateUrl: 'about_component.html',
+    styleUrls: const ['about_component.css'])
+class AboutComponent {}
