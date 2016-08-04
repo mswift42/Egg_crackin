68052c44f924d0f04b51d2ecb667f880f28fb33d
mswift42 <martin.haesler@gmail.com>
2016-08-04 17:21:39 +0100

    Add router links to about / favourites / home

--- lib/components/navbar-component/navbar_component.dart
+++ lib/components/navbar-component/navbar_component.dart
@@ -2,19 +2,18 @@ import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
 import 'package:eggcrackin/components/recipe-component/recipe_component.dart';
 import 'package:eggcrackin/components/favourite-component/favourite_component.dart';
+import 'package:eggcrackin/components/about-component/about_component.dart';
 
 @Component(
     selector: 'navbar-component',
     templateUrl: 'navbar_component.html',
-    styleUrls: const ['navbar_component.css'],
-    directives: const [ROUTER_DIRECTIVES])
-@RouteConfig(const [
-  const Route(
-      path: '/',
-      name: 'Recipes',
-      component: RecipeComponent,
-      useAsDefault: true),
-  const Route(
-      path: '/favourites', name: 'Favourites', component: FavouriteComponent)
-])
+    styleUrls: const [
+      'navbar_component.css'
+    ],
+    directives: const [
+      ROUTER_DIRECTIVES,
+      RecipeComponent,
+      FavouriteComponent,
+      AboutComponent
+    ])
 class NavbarComponent {}
