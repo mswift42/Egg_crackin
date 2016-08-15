6be8f6e422446df4b604af79fc40a56d69b95e6c
mswift42 <martin.haesler@gmail.com>
2016-08-15 20:07:15 +0100

    Add Class RecipesViewComponent

---
+++ lib/components/recipesview-component/recipesview.component.dart
@@ -0,0 +1,7 @@
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'recipesview-component',
+    templateUrl: 'recipesview.component.html',
+    styleUrls: const ['recipesview.component.css'])
+class RecipesViewComponent {}
