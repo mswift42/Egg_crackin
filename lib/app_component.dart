55e9be6b86f679984d1acc01ae5ee7a65a58dc35
mswift42 <martin.haesler@gmail.com>
2016-08-16 21:19:04 +0100

    import favouritelist component for favourites view

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -4,9 +4,9 @@ import 'package:eggcrackin/components/navbar-component/navbar_component.dart';
 import 'package:eggcrackin/services/RecipeService.dart';
 import 'package:eggcrackin/services/FavouritesService.dart';
 import 'package:eggcrackin/services/SearchhistoryService.dart';
-import 'package:eggcrackin/components/favourite-component/favourite_component.dart';
 import 'package:eggcrackin/components/about-component/about_component.dart';
 import 'package:eggcrackin/components/recipe-component/recipe_component.dart';
+import 'package:eggcrackin/components/favouritelist-component/favouritelist_component.dart';
 
 @Component(
     selector: 'my-app',
@@ -28,7 +28,7 @@ import 'package:eggcrackin/components/recipe-component/recipe_component.dart';
       component: RecipeComponent,
       useAsDefault: true),
   const Route(
-      path: '/favourites', name: 'Favourites', component: FavouriteComponent),
+      path: '/favourites', name: 'Favourites', component: FavouritelistComponent),
   const Route(path: '/about', name: 'About', component: AboutComponent)
 ])
 class AppComponent {}
