2d241dac9c5c5319403c40b47284e590b7468299
mswift42 <martin.haesler@gmail.com>
2016-08-17 16:45:48 +0100

    Fix from Json conversion in loadFromStorage method

--- lib/services/FavouritesService.dart
+++ lib/services/FavouritesService.dart
@@ -7,19 +7,24 @@ import 'package:eggcrackin/services/Recipe.dart';
 class FavouritesService {
   Storage localStorage = window.localStorage;
   final _storagename = "recipes";
-  List<Recipe> favourites;
+  List<Recipe> favourites = [];
 
   void saveToStorage(Recipe recipe) {
     var recipes = [];
     if (localStorage[_storagename] != null) {
       recipes = JSON.decode(localStorage[_storagename]);
     }
-    recipes.add(JSON.encode({"title" : recipe.title, "publisher" : recipe.publisher,
-    "publisher_url" : recipe.publisher_url, "source_url" : recipe.source_url,
-    "image_url" : recipe.source_url}));
+    recipes.add(JSON.encode({
+      "title": recipe.title,
+      "publisher": recipe.publisher,
+      "publisher_url": recipe.publisher_url,
+      "source_url": recipe.source_url,
+      "image_url": recipe.source_url
+    }));
     localStorage[_storagename] = JSON.encode(recipes);
     print(JSON.decode(localStorage[_storagename]));
   }
+
   void deleteInStorage(Recipe recipe) {
     var recipes = [];
     if (localStorage[_storagename] != null) {
@@ -28,14 +33,17 @@ class FavouritesService {
     recipes = recipes.where((i) => i["source_url"] != recipe.source_url);
     localStorage[_storagename] = JSON.encode(recipes);
   }
+
   List<Recipe> loadFromStorage() {
     if (localStorage[_storagename] != null) {
       List<Map> favs = JSON.decode(localStorage[_storagename]);
-      favs.forEach((i) => favourites.add(new Recipe(i["publisher"], i["title"],
-          i["source_url"], i["image_url"], i["publisher_url"])));
+      favs.forEach((i) => favourites.add(new Recipe(
+          JSON.decode(i)["publisher"],
+          JSON.decode(i)["title"],
+          JSON.decode(i)["source_url"],
+          JSON.decode(i)["image_url"],
+          JSON.decode(i)["publisher_url"])));
     }
     return favourites;
   }
 }
-
-
