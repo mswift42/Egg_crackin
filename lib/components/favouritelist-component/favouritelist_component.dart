96705da623138fc565454b0ce1c79db76d237902
mswift42 <martin.haesler@gmail.com>
2016-08-17 17:20:38 +0100

    use recipeimage component for favourites

--- lib/components/favouritelist-component/favouritelist_component.dart
+++ lib/components/favouritelist-component/favouritelist_component.dart
@@ -4,13 +4,20 @@ import 'package:eggcrackin/services/FavouritesService.dart';
 import 'package:eggcrackin/components/title-component/title_component.dart';
 import 'package:eggcrackin/components/publisherurl-component/publisherurl_component.dart';
 import 'package:eggcrackin/components/recipesview-component/recipesview.component.dart';
+import 'package:eggcrackin/components/recipeimage-component/recipeimage_component.dart';
 
 @Component(
     selector: 'favouritelist-component',
     templateUrl: 'favouritelist_component.html',
-styleUrls: const ['favouritelist_component.css'],
-directives: const [TitleComponent, PublisherurlComponent, RecipesViewComponent]
-)
+    styleUrls: const [
+      'favouritelist_component.css'
+    ],
+    directives: const [
+      TitleComponent,
+      PublisherurlComponent,
+      RecipeimageComponent,
+      RecipesViewComponent
+    ])
 class FavouritelistComponent {
   List<Recipe> favourites;
 
