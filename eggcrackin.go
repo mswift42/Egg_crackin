1ef63f24e582424c700cf029db921f8b013e987a
mswift42 <martin.haesler@gmail.com>
2016-08-20 18:55:25 +0100

    Add go handler functions

---
+++ eggcrackin.go
@@ -0,0 +1,41 @@
+package eggcrackin
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
+)
+
+func init() {
+	http.HandleFunc("/searchrecipe", getRecipes)
+}
+
+const (
+	apiKey  = "7987c43afcf8a03a964bbcb0c9152c84"
+	baseURL = "http://food2fork.com/api/search?key="
+)
+
+func getRecipes(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	query := r.FormValue("query")
+	url := searchURL(query)
+	ctx := appengine.NewContext(r)
+	client := urlfetch.Client(ctx)
+	recipes, err := client.Get(url)
+	if err != nil {
+		fmt.Fprintf(w, err.Error(), http.StatusInternalServerError)
+	}
+	body, err := ioutil.ReadAll(recipes.Body)
+	if err != nil {
+		http.Error(w, "couldn't read get request body", http.StatusInternalServerError)
+	}
+	fmt.Fprintf(w, string(body))
+}
+
+func searchURL(query string) string {
+	return baseURL + apiKey + "&q=" + strings.Replace(query, " ", "+", -1)
+}
