065daee421dcbace8d0204b2b90ea43f89c00d43
mswift42 <martin.haesler@gmail.com>
2016-08-21 15:04:05 +0100

    Add main.dart

---
+++ web/main.dart
@@ -0,0 +1,8 @@
+import 'package:angular2/platform/browser.dart';
+import 'package:angular2/router.dart';
+
+import 'package:eggcrackin/app_component.dart';
+
+main() {
+  bootstrap(AppComponent, [ROUTER_PROVIDERS]);
+}
