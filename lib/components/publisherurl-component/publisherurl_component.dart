f02058239918aa01a1fc1e4e6daf4c29d0982c03
mswift42 <martin.haesler@gmail.com>
2016-08-21 09:47:46 +0100

    Delete empty publisherurl component css file

--- lib/components/publisherurl-component/publisherurl_component.dart
+++ lib/components/publisherurl-component/publisherurl_component.dart
@@ -2,11 +2,10 @@ import 'package:angular2/core.dart';
 
 @Component(
     selector: 'publisherurl-component',
-templateUrl: 'publisherurl_component.html',
-styleUrls: const ['publisherurl_component.css']
-)
-
+    templateUrl: 'publisherurl_component.html')
 class PublisherurlComponent {
-  @Input() String publisher;
-  @Input() String publisher_url;
+  @Input()
+  String publisher;
+  @Input()
+  String publisher_url;
 }
