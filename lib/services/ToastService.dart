c88623c9a14c9aee32f31a6e26f46d4d2d041469
mswift42 <martin.haesler@gmail.com>
2016-08-26 22:09:04 +0100

    Add setter / getter for _show.

--- lib/services/ToastService.dart
+++ lib/services/ToastService.dart
@@ -9,4 +9,8 @@ class ToastService {
     _show = true;
     var timer = new Timer(const Duration(seconds: 3), () => _show = false);
   }
+
+  get show => _show;
+
+  set show(bool status) => _show = status;
 }
