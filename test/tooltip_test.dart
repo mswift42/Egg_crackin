cd435607cf9361cda2c363e0d6a10ea76576b4ab
mswift42 <martin.haesler@gmail.com>
2016-09-05 16:34:57 +0100

    Add file tooltip_test

---
+++ test/tooltip_test.dart
@@ -0,0 +1,10 @@
+import 'package:angular2/core.dart';
+import 'package:angular2/testing.dart';
+
+import 'package:eggcrackin/components/tooltip-component/tooltip_component.dart';
+@TestOn('browser')
+import 'package:test/test.dart';
+
+void main() {
+
+}
