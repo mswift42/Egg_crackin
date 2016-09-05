a2c6bb84c202e52fda0d9a8f903390fd26e42a40
mswift42 <martin.haesler@gmail.com>
2016-09-05 12:27:51 +0100

    emit bool event for toastservice

--- lib/components/bookmark-component/bookmark_component.dart
+++ lib/components/bookmark-component/bookmark_component.dart
@@ -2,30 +2,34 @@ import 'package:angular2/core.dart';
 import 'package:eggcrackin/services/FavouritesService.dart';
 import 'package:eggcrackin/services/Recipe.dart';
 import 'package:eggcrackin/components/tooltip-component/tooltip_component.dart';
+import 'package:eggcrackin/components/toast-component/toast.component.dart';
 
 @Component(
     selector: 'bookmark-component',
     templateUrl: 'bookmark_component.html',
     styleUrls: const ['bookmark_component.css'],
-    directives: const [TooltipComponent])
-  class BookmarkComponent {
-    @Input()
-      String title;
-    @Input()
-      String publisher;
-    @Input()
-      String publisher_url;
-    @Input()
-      String source_url;
-    @Input()
-      String image_url;
+    directives: const [TooltipComponent, ToastComponent])
+class BookmarkComponent {
+  @Input()
+  String title;
+  @Input()
+  String publisher;
+  @Input()
+  String publisher_url;
+  @Input()
+  String source_url;
+  @Input()
+  String image_url;
+  @Output()
+  EventEmitter onShow = new EventEmitter();
 
-    final FavouritesService _favouritesService;
+  final FavouritesService _favouritesService;
 
-    BookmarkComponent(this._favouritesService);
+  BookmarkComponent(this._favouritesService);
 
-    void bookmarkRecipe() {
-      _favouritesService.saveToStorage(
-          new Recipe(publisher, title, source_url, image_url, publisher_url));
-    }
+  void bookmarkRecipe() {
+    _favouritesService.saveToStorage(
+        new Recipe(publisher, title, source_url, image_url, publisher_url));
+    onShow.emit(true);
   }
+}
