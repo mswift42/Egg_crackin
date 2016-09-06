e39961c8f227f4bf41ebb2f0c47899d95f968f95
mswift42 <martin.haesler@gmail.com>
2016-09-06 17:54:29 +0100

    Add constructor fromJsonMap

--- lib/services/Recipe.dart
+++ lib/services/Recipe.dart
@@ -1,6 +1,22 @@
+import 'dart:convert';
+
 class Recipe {
   String publisher, title, source_url, image_url, publisher_url;
 
   Recipe(this.publisher, this.title, this.source_url, this.image_url,
       this.publisher_url);
+
+  String _unsanitize(String s) {
+    s = s.replaceAll("&nbsp;", " ");
+    s = s.replaceAll("&#8217;", "'");
+    return s.replaceAll("&amp;", "&");
+  }
+
+  Recipe.fromJsonMap(Map<String, String> recipemap) {
+    publisher = recipemap["publisher"];
+    title = _unsanitize(recipemap["title"]);
+    source_url = recipemap["source_url"].toString();
+    image_url = recipemap["image_url"];
+    publisher_url = recipemap["publisher_url"];
+  }
 }
