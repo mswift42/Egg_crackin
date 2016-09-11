fd282b1df43b8434b0b43f338e2886e0a3c1071e
mswift42 <martin.haesler@gmail.com>
2016-09-11 12:24:30 +0100

    start test for tooltip component

--- test/tooltip_test.dart
+++ test/tooltip_test.dart
@@ -1,10 +1,24 @@
-import 'package:angular2/core.dart';
-import 'package:angular2/testing.dart';
 
-import 'package:eggcrackin/components/tooltip-component/tooltip_component.dart';
 @TestOn('browser')
+import 'package:angular2/testing_internal.dart';
 import 'package:test/test.dart';
 
+import 'dart:html';
+
+import 'package:eggcrackin/components/tooltip-component/tooltip_component.dart';
+
 void main() {
+  test('tooltip', () {
+    return inject([TestComponentBuilder, AsyncTestCompleter],
+    (TestComponentBuilder tcb, AsyncTestCompleter completer) {
+      tcb.createAsync(TooltipComponent).then((cmp) {
+        expect(document.body.text, isNot(contains("Tooltip Message")));
+        cmp.componentInstance.message = "Tooltip Message";
+        cmp.detectChanges();
+        expect(document.body.text, contains('Tooltip Message'));
+        completer.done();
+      });
+    });
+  });
 
 }
